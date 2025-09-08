042647e9c27a3aaf7b71a7ded1492cc8dac517f5
STONE-GPY <smj00316@gmail.com>
2025-09-08 17:17:54 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using HeyRed.Mime;
+
+namespace GetMime
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.GetEnvironmentVariable("PATH"));
+            if (args.Length != 1)
+            {
+                Console.WriteLine($"Usage: GetMime.exe <path-to-file>");
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"File not found: {args[0]}");
+                return;
+            }
+            
+            var result = MimeGuesser.GuessMimeType(args[0]);
+            Console.WriteLine("Guessed mime type : " + result);
+            return;
+        }
+    }
+}
